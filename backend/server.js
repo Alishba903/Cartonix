1ef4feed8d02a70cc7607f21a762d0fdc134c606
Alishba <a.alishba1002@gmail.com>
2025-07-30 16:35:10 +0500

    Setting up project

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("Server is Ready");
+});
+
 app.listen(5000, () => {
-    console.log('Server is ');
+    console.log('Server started at http://localhost:5000');
 })
