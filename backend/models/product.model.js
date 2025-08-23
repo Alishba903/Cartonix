ee4e1822c7c7e20104060f581de04a90676e2ca7
Alishba <a.alishba1002@gmail.com>
2025-08-23 08:40:24 +0500

    worked on some files

---
+++ backend/models/product.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+
+    },
+    image: {
+        type: String,
+        required: true
+    }
+}, {
+    timeStamps: true // Automatically adds createdAt and updatedAt fields
+});
+
+const Product = mongoose.model('Product', productSchema);
+// Mongoose will convert the Product into products collection in MongoDB. It just want you write like this with singular and capitalize lette mongoose will the rest for you.
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,34 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
+import Product from './models/product.model.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("Server is Ready");
+app.use(express.json()); // Middleware to parse JSON bodies. allows us to accept json data in the req.body.
+
+app.post("/api/products", async (req, res)=>{
+    const product = req.body; // user will send this data 
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success: false, message: "All fields are required"});
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        new Product.save();
+        res.status(201).json({success: true, message: "Product created successfully", data: newProduct})
+    }catch(error){
+        console.log("Error in create Product", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
 });
 
+
 app.listen(5000, () => {
+    connectDB();
     console.log('Server started at http://localhost:5000');
 })
