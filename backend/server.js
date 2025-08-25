0554145e3a8ab1767fc46fa7907ecdd15c0a83b6
Alishba <a.alishba1002@gmail.com>
2025-08-25 08:44:01 +0500

    Added some api

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,15 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies. allows us to accept json data in the req.body.
 
-app.get('/', (req, res) => {
-       res.send('Hello World!');
-   });
+app.get("/api/products", async (req, res)=>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products});
+    }catch(error){
+        console.log("error in fetching products:", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
+})
 
 app.post("/api/products", async (req, res)=>{
     const product = req.body; // user will send this data 
@@ -30,6 +36,18 @@ app.post("/api/products", async (req, res)=>{
         res.status(500).json({success: false, message: "Server Error"});
     }
 });
+
+app.delete("/api/products/:id", async (req, res)=>{
+    const {id} = req.params; 
+    console.log("id:", id);
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted succesfully."})
+    }catch(error){
+        console.log("error in deleting product:", error.message);
+        res.status(404).json({success: false, message: "Product not found"});
+    }
+})
 
 
 app.listen(5000, () => {
