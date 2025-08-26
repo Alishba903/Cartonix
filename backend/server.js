ef95b99e200f568706903610c67436fb69594af0
Alishba <a.alishba1002@gmail.com>
2025-08-26 08:05:57 +0500

    Working with api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js'
 
 dotenv.config();
 
@@ -9,46 +9,7 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies. allows us to accept json data in the req.body.
 
-app.get("/api/products", async (req, res)=>{
-    try{
-        const products = await Product.find({});
-        res.status(200).json({success: true, data: products});
-    }catch(error){
-        console.log("error in fetching products:", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-})
-
-app.post("/api/products", async (req, res)=>{
-    const product = req.body; // user will send this data 
-
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success: false, message: "All fields are required"});
-    }
-
-    const newProduct = new Product(product);
-
-    try{
-        newProduct.save();
-        res.status(201).json({success: true, message: "Product created successfully", data: newProduct})
-    }catch(error){
-        console.log("Error in create Product", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.delete("/api/products/:id", async (req, res)=>{
-    const {id} = req.params; 
-    console.log("id:", id);
-    try{
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product deleted succesfully."})
-    }catch(error){
-        console.log("error in deleting product:", error.message);
-        res.status(404).json({success: false, message: "Product not found"});
-    }
-})
-
+app.use("/api/products", productRoutes)
 
 app.listen(5000, () => {
     connectDB();
