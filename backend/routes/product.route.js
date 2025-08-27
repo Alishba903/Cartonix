7d012ba5a35846bbb027b97a678b308642a2d91f
Alishba <a.alishba1002@gmail.com>
2025-08-27 08:08:08 +0500

    Done with Backend

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import {createProduct, deleteProduct, getProducts, updateProduct} from "../controllers/product.controller"
+import {createProduct, deleteProduct, getProducts, updateProduct} from "../controllers/product.controller.js"
 
 const router = express.Router();
 
